b5e9e738803f5d65ffe9328ccb0aca5c4390c1f9
elsyng <amstel01@gmail.com>
2021-01-09 17:28:43 +0100

    first commit

---
+++ precache-manifest.328c5abc5bd0ff9368bc274a61a76fc3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b1532dfc32ff30b97cfe4201e564f4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0f2f919ac017525365d",
+    "url": "/static/css/main.33076848.chunk.css"
+  },
+  {
+    "revision": "efaa4841f92db4fbbbea",
+    "url": "/static/js/2.af503ffb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.af503ffb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0f2f919ac017525365d",
+    "url": "/static/js/main.05abd301.chunk.js"
+  },
+  {
+    "revision": "34084078d94fbd309ed6",
+    "url": "/static/js/runtime-main.6bab6a54.js"
+  },
+  {
+    "revision": "a666a3dc2358b75baa206b2dd4c5a615",
+    "url": "/static/media/corbel.a666a3dc.ttf"
+  }
+]);
